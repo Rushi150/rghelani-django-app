64017b6f6aa41ce3e8ee18241264fb0da74f00d6
Rushi150 <rushighelanica@gmail.com>
2025-03-28 16:29:50 -0400

    base html css and js files created

--- OneDrive/Desktop/src/posts_proj/settings.py
+++ OneDrive/Desktop/src/posts_proj/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posts'
+    'profiles',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'posts_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
